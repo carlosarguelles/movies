3a5a726b51a41d50185f9ddd962091071d98c749
carlosarguelles <carlosnapx@gmail.com>
2022-04-08 02:57:22 -0500

    feat(utils): using reactive params
    https://vuejs.org/guide/reusability/composables.html#async-state-example

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,18 +1,35 @@
-import { ref } from 'vue'
+import { isRef, ref, Ref, watchEffect } from 'vue'
 
-export function useFetch<T>(url: string) {
-  const data = ref<T>()
+export function useFetch<T>(url: string, params: Ref<Record<string, any>>) {
+  const data = ref<T | null>()
   const error = ref()
 
-  fetch(import.meta.env.VITE_API_URL + url, {
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(res => res.json())
-    .then(json => (data.value = json))
-    .catch(err => (error.value = err))
+  function doFetch() {
+    data.value = null
+    error.value = null
+
+    let reqUrl = import.meta.env.VITE_API_URL + url
+
+    if (params) {
+      reqUrl += '?' + new URLSearchParams(params.value).toString()
+    }
+
+    fetch(reqUrl, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(json => (data.value = json))
+      .catch(err => (error.value = err))
+  }
+
+  if (isRef(params)) {
+    watchEffect(doFetch)
+  } else {
+    doFetch()
+  }
 
   return { data, error }
 }
