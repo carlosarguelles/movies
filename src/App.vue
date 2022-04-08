7fb1d3ae51e8a01298021da0fca4a6bea1699f9e
carlosarguelles <carlosnapx@gmail.com>
2022-04-08 02:59:07 -0500

    styling stuff

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,15 @@
 </template>
 
 <style>
+* {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+
 body {
   background-color: black;
   color: white;
+  max-width: 1000px;
+  margin: auto;
 }
 </style>
