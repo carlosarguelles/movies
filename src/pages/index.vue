d9cfd33d93fb047eb6bb3ea43523101e36bc17f4
carlosarguelles <carlosnapx@gmail.com>
2022-04-08 00:44:08 -0500

    feat: fetching movies and showing in card component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { useFetch } from '../utils/fetch'
+import Card from '../components/Card.vue'
+import { PopularMovies } from '../types/PopularMovies'
 
-let count = ref(0)
+const { data, error } = useFetch<PopularMovies>('/movie/popular')
 </script>
 
 <template>
   <h1>movies</h1>
+  <div v-if="error">Oops! Error encountered: {{ error.message }}</div>
+  <div v-else-if="data">
+    <div v-for="movie in data.results">
+      <Card :movie="movie" />
+    </div>
+  </div>
+  <div v-else>Loading...</div>
 </template>
