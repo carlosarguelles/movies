0ffe98b377ace14c756016876a1c01c9c07bf27f
carlosarguelles <carlosnapx@gmail.com>
2022-04-08 02:59:30 -0500

    feat(components): added image to movie card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,7 +6,7 @@ const { movie } = defineProps<{ movie: Movie }>()
 
 <template>
   <article>
-    <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" />
+    <img :src="`https://image.tmdb.org/t/p/w200/${movie.poster_path}`" />
     <h2>{{ movie.title }}</h2>
   </article>
 </template>
@@ -14,5 +14,20 @@ const { movie } = defineProps<{ movie: Movie }>()
 <style scoped>
 h2 {
   color: white;
+  text-transform: uppercase;
+}
+
+h2:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+article {
+  padding: 10px;
+  text-align: center;
+}
+
+img {
+  border-radius: 10px;
 }
 </style>
