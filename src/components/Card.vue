29e84c8d64c16d7a154883a9f95ee86e077a82a3
carlosarguelles <carlosnapx@gmail.com>
2022-04-08 00:49:21 -0500

    feat: added Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Movie } from '../types/Movie'
+
+const { movie } = defineProps<{ movie: Movie }>()
+</script>
+
+<template>
+  <article>
+    <img :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`" />
+    <h2>{{ movie.title }}</h2>
+  </article>
+</template>
+
+<style scoped>
+h2 {
+  color: white;
+}
+</style>
