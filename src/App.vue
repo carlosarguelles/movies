c3a6f38348642ca1cb08b74950f9c3bc355ba58b
carlosarguelles <carlosnapx@gmail.com>
2022-04-09 14:47:34 -0500

    some styling

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,49 @@ body {
   max-width: 1000px;
   margin: auto;
 }
+
+a {
+  font-size: 24px;
+  margin-bottom: 15px;
+  margin-top: 15px;
+  font-weight: 700;
+  display: block;
+  color: white;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+select,
+button {
+  border: none;
+  outline: none;
+  font-size: 20px;
+  background: none;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover {
+  text-decoration: underline;
+}
+
+button:disabled {
+  cursor: unset;
+  text-decoration: none;
+  color: gray;
+}
+
+button:focus {
+  border: none;
+  outline: none !important;
+}
+
+img {
+  border-radius: 10px;
+}
 </style>
