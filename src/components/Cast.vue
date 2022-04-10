6471129de3fbd6cf9c9900848bce731d952ded96
carlosarguelles <carlosnapx@gmail.com>
2022-04-10 00:33:37 -0500

    feat: added main cast

---
+++ src/components/Cast.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { Cast } from '../types/Credits'
+
+const { cast } = defineProps<{ cast: Cast }>()
+</script>
+
+<template id="card">
+  <div>
+    <img
+      :src="`https://image.tmdb.org/t/p/w200/${cast.profile_path}`"
+      :alt="cast.name"
+    />
+    <p>{{ cast.name }}</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  text-align: center;
+  flex: 1 1 0px;
+}
+
+img {
+  border-radius: 50%;
+  object-fit: cover;
+  width: 100px;
+  height: 100px;
+}
+</style>
--- src/pages/movie.vue
+++ src/pages/movie.vue
@@ -2,9 +2,11 @@
 import { useRoute, useRouter } from 'vue-router'
 import { useFetch } from '../utils/useFetch'
 import { DetailedMovie } from '../types/DetailedMovie'
-import { computed } from '@vue/reactivity';
-import { watchEffect } from 'vue';
+import { Credits } from '../types/Credits'
+import { computed } from '@vue/reactivity'
+// import { watchEffect } from 'vue';
 import Imdb from '../components/Imdb.vue'
+import Cast from '../components/Cast.vue'
 
 const router = useRouter()
 const route = useRoute()
@@ -12,13 +14,16 @@ const route = useRoute()
 let movieId = route.params.id
 
 const { data: movie, error } = useFetch<DetailedMovie>(`/movie/${movieId}`)
+const { data: credits, error: _ } = useFetch<Credits>(`/movie/${movieId}/credits`)
 
-watchEffect(
-  () => console.log(movie.value)
-)
+// watchEffect( () => console.log(movie.value))
 
 const genres = computed(() => {
-  return movie.value.genres.map(genre => genre.name).join(', ')
+  return movie?.value?.genres?.map(genre => genre.name).join(', ')
+})
+
+const mainCast = computed(() => {
+  return credits.value?.cast?.slice(0, 5)
 })
 </script>
 
@@ -31,10 +36,16 @@ const genres = computed(() => {
       <h1>{{ movie.title }}</h1>
       <p>Overview: {{ movie.overview }}</p>
       <p>Genres: {{ genres }}</p>
-      <p>Release date: {{ movie.release_date.toString() }}</p>
+      <p>Release date: {{ movie?.release_date?.toString() }}</p>
       <a :href="`https://www.imdb.com/title/${movie.imdb_id}`" target="_blank">
         <Imdb style="width: 50px;"></Imdb>
       </a>
+      <div v-if="credits">
+        <div style="margin-bottom: 20px;">Main cast:</div>
+        <div id="cast">
+          <Cast :cast="cast" v-for="cast in mainCast" />
+        </div>
+      </div>
     </div>
   </div>
   <div v-else>Loading...</div>
@@ -54,6 +65,14 @@ h1 {
 #movie {
   display: flex;
   gap: 30px;
+  align-items: flex-start;
+}
+
+#cast {
+  display: flex;
+  gap: 15px;
+  flex-wrap: nowrap;
+  overflow-x: auto;
 }
 
 p {
